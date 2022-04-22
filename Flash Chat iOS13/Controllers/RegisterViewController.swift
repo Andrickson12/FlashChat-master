1094e29aa66c1dff6b8fcc5797030d1e27476245
Andrickson12 <Andrickson12>
2022-04-22 00:56:21 -0400

    Complete user authentication, login and sign out implemented in a full scalable infrastructure

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text{
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                
+                // Print error if happens
+                if let e = error {
+                    print(e)
+                } else {
+                    // Navigate to chatViewController
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+                
+            }
+        }
+        
     }
     
 }
